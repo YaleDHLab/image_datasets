ee94bbd3dff413ea04b4c47ed32c77a37bc35d3c
Peter Leonard <pleonard@gmail.com>
2020-03-30 13:02:45 -0400

    commas are important

--- image_datasets/__init__.py
+++ image_datasets/__init__.py
@@ -1,4 +1,4 @@
-from image_datasets.datasets import oslo, oslomini, bain, fsaowi_ct list_all
+from image_datasets.datasets import oslo, oslomini, bain, fsaowi_ct, list_all
 import sys
 
 setattr(sys.modules[__name__], 'list', list_all)
